b9ca64a5cbeb1fe28955f4f8927a6e7297ae4927
JarekSopinski <jarek.sopinski@gmail.com>
2018-04-16 11:28:45 +0200

    API url settings

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
+let searchTerm;
+let searchLimit = "10";
+const API_URL_END = "https://en.wikipedia.org/w/api.php?";
+const API_URL_SETTINGS = `format=json&action=query&generator=search&gsrnamespace=0&prop=pageimages|extracts|description&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrlimit=${searchLimit}&gsrsearch=gravity`;
+
 
 
