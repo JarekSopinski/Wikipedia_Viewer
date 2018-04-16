3174fc471b7f8e45fcd9edab97165840ea1d5410
JarekSopinski <jarek.sopinski@gmail.com>
2018-04-16 12:28:30 +0200

    Succesful ajax call on a test search term

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-let searchTerm;
+const $searchBtn = $("#searchBtn");
+
+let searchTerm = "";
 let searchLimit = "10";
 const API_URL_END = "https://en.wikipedia.org/w/api.php?";
-const API_URL_SETTINGS = `format=json&action=query&generator=search&gsrnamespace=0&prop=pageimages|extracts|description&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrlimit=${searchLimit}&gsrsearch=gravity`;
+const API_URL_SETTINGS = `format=json&formatversion=2&action=query&generator=search&gsrnamespace=0&prop=pageimages|extracts|description&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrlimit=${searchLimit}&gsrsearch=`;
+const ERROR_MSG = "An error occurred. Please check your internet connection and try again.";
 
+const fetchDataFromWikipedia = () => {
 
+    searchTerm = "meaning";
 
+    return $.ajax({
+        method: "GET",
+        dataType: "jsonp",
+        url: `${API_URL_END}${API_URL_SETTINGS}${searchTerm}`
+    })
+
+};
+
+const performSearch = () => {
+
+    fetchDataFromWikipedia()
+        .then(data => console.log(data.query.pages))
+        .catch(handleError)
+
+};
+
+const handleError = () => {
+    alert(ERROR_MSG)
+};
+
+$searchBtn.on("click", performSearch);
