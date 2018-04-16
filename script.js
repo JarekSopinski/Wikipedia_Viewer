9d70629357b29c52fb083891b1dc847164182fb0
JarekSopinski <jarek.sopinski@gmail.com>
2018-04-16 13:42:46 +0200

    Handling input's whitespaces

--- script.js
+++ script.js
@@ -9,10 +9,18 @@ const API_URL = "https://en.wikipedia.org/w/api.php?";
 const API_SETTINGS = `format=json&formatversion=2&action=query&generator=search&gsrnamespace=0&prop=pageimages|extracts|description&pithumbsize=300&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max`;
 const ERROR_MSG = "An error occurred. Please check your internet connection and try again.";
 
+const performSearch = () => {
+
+    fetchDataFromWikipedia()
+        .then(data => console.log(data))
+        .catch(handleError)
+
+};
+
 const fetchDataFromWikipedia = () => {
 
     searchLimit = $searchSelect.val();
-    searchTerm = $searchInput.val();
+    searchTerm = handleInputWhitespaces($searchInput.val());
 
     console.log(`${API_URL}${API_SETTINGS}&gsrlimit=${searchLimit}&gsrsearch=${searchTerm}`);
 
@@ -23,16 +31,12 @@ const fetchDataFromWikipedia = () => {
 
 };
 
-const performSearch = () => {
-
-    fetchDataFromWikipedia()
-        .then(data => console.log(data))
-        .catch(handleError)
-
-};
-
 const handleError = () => {
     alert(ERROR_MSG)
+};
+
+const handleInputWhitespaces = (inputValue) => {
+    return inputValue.replace(/ /g, "+")
 };
 
 $searchBtn.on("click", performSearch);
